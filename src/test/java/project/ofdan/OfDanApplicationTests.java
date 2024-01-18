00e06f1df21f58c778c3cb2cc544f6a8049d5477
Choiyu330 <jenny4063@naver.com>
2024-01-19 02:37:59 +0900

    test: 회원 서비스 테스트

---
+++ src/test/java/project/ofdan/OfDanApplicationTests.java
@@ -0,0 +1,13 @@
+package project.ofdan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfDanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

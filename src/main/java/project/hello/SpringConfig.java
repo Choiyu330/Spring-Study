186159e14d1d58190012a0670d4f88885d97df0e
Choiyu330 <jenny4063@naver.com>
2024-01-22 00:21:40 +0900

    feat: 스프링 JdbcTemplate 구현

--- src/main/java/project/hello/SpringConfig.java
+++ src/main/java/project/hello/SpringConfig.java
@@ -1,13 +1,23 @@
 package project.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import project.hello.repository.JdbcTemplateMemberRepository;
 import project.hello.repository.MemberRepository;
-import project.hello.repository.MemoryMemberRepository;
 import project.hello.service.MemberService;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }

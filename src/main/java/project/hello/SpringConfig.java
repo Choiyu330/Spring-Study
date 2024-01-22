9a6bb553212fbd62d6313ad50c6f9fa438e127fd
Choiyu330 <jenny4063@naver.com>
2024-01-22 23:04:37 +0900

    feat: Spring Data JPA 구현

--- src/main/java/project/hello/SpringConfig.java
+++ src/main/java/project/hello/SpringConfig.java
@@ -1,29 +1,29 @@
 package project.hello;
 
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import project.hello.repository.JpaMemberRepository;
 import project.hello.repository.MemberRepository;
 import project.hello.service.MemberService;
 
 @Configuration
 public class SpringConfig {
-    private EntityManager em;
+
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        return new JpaMemberRepository(em);
-    }
+//    Spring Data JPA 를 사용하면서 더이상 사용되지 않음
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new JpaMemberRepository(em);
+//    }
 }
